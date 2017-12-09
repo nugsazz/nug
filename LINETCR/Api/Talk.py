ee42646b992de690db7dd1ae1a4d9ac82a7841e5
nugi <30439272+nugsazz@users.noreply.github.com>
2017-12-10 01:51:56 +0700

    Update Talk.py

--- LINETCR/Api/Talk.py
+++ LINETCR/Api/Talk.py
@@ -20,8 +20,9 @@ class Talk:
   host = "gd2.line.naver.jp";
   port = 443;
 
-  UA = "Line/6.0.0 iPad4,1 9.0.2"
-  LA = "DESKTOPMAC 10.10.2-YOSEMITE-x64    MAC 4.5.0"
+  UA = "Line/7.15.1"
+  LA = "IOSIPAD\t7.15.1\tiPhone OS\t10.12.0"
+
 
   authToken = None
   cert = None
